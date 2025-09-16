7634dab2aea2aea3e3c3bd5b79989a6fbc4854cb
Leezy <191919qaz@gmail.com>
2025-09-16 17:00:12 +0300

    add controller

--- brick_game/snake/model/game.cpp
+++ brick_game/snake/model/game.cpp
@@ -1,7 +1,11 @@
 #include "game.h"
 
 s21::Game::Game() {
-    Info.field[FIELD_WIDTH][FIELD_HEIGHT];
+    Info.field = new int* [FIELD_HEIGHT];
+    for (int i = 0; i < FIELD_HEIGHT; i++) {
+        Info.field[i] = new int[FIELD_WIDTH]();
+    }
+
     Info.score = 0;
     Info.high_score = 0;
     Info.level = 0;
